1edb050185d16420108cb07b8ff57fe800ab662b
Michal Špaček <tupinek@gmail.com>
2012-09-08 22:14:21 +0200

    New version.

--- Keyboard.pm
+++ Keyboard.pm
@@ -10,6 +10,7 @@ use Config::Dot;
 use Encode qw(decode_utf8);
 use Error::Pure qw(err);
 use File::Spec::Functions qw(catfile);
+use HTML::Entities;
 use Imager;
 use Perl6::Slurp qw(slurp);
 
@@ -34,13 +35,22 @@ sub new {
 		err "Bad files directory '$self->{'files_dir'}'.";
 	}
 
+	# Buttons.
+	$self->{'buttons'} = undef;
+
 	# Object.
 	return $self;
 }
 
+# Get buttons count.
+sub buttons {
+	my $self = shift;
+	return $self->{'buttons'};
+}
+
 # Create image.
 sub image {
-	my ($self, $image, $config_file) = @_;
+	my ($self, $config_file, $image, $type) = @_;
 	
 	# Process config.
 	my $config = slurp($config_file);
@@ -50,14 +60,15 @@ sub image {
 			return decode_utf8($value);
 		},
 	);
-	my $c_hr = $c->parse($config);
+	$self->{'c'} = $c->parse($config);
 
 	# Create image.
-	if (! $c_hr->{'background'}) {
+	if (! $self->{'c'}->{'background'}) {
 		err 'No background image.';
 	}
 	$self->{'i'} = Imager->new(
-		'file' => catfile($self->{'files_dir'}, $c_hr->{'background'}),
+		'file' => catfile($self->{'files_dir'},
+			$self->{'c'}->{'background'}),
 	);
 	if (! defined $self->{'i'}) {
 		err 'Cannot create background image',
@@ -68,8 +79,10 @@ sub image {
 	($self->{'width'}, $self->{'height'}) = $self->_size($self->{'i'});
 
 	# Add all buttons.
-	foreach my $button_nr (sort keys %{$c_hr->{'button'}}) {
-		my $b_hr = $c_hr->{'button'}->{$button_nr};
+	my @buttons = sort keys %{$self->{'c'}->{'button'}};
+	$self->{'buttons'} = scalar @buttons;
+	foreach my $button_nr (@buttons) {
+		my $b_hr = $self->{'c'}->{'button'}->{$button_nr};
 		my $image_path = catfile($self->{'files_dir'},
 			$b_hr->{'image'});
 		my $i = Imager->new('file' => $image_path);
@@ -83,25 +96,25 @@ sub image {
 		if (exists $b_hr->{'font'}->{'file'}) {
 			$font_file = catfile($self->{'files_dir'},
 				$b_hr->{'font'}->{'file'});
-		} elsif (exists $c_hr->{'font'}->{'file'}) {
+		} elsif (exists $self->{'c'}->{'font'}->{'file'}) {
 			$font_file = catfile($self->{'files_dir'},
-				$c_hr->{'font'}->{'file'});
+				$self->{'c'}->{'font'}->{'file'});
 		} else {
 			err "No font file for button '$button_nr'.";
 		}
 		my $font_color;
 		if (exists $b_hr->{'font'}->{'color'}) {
 			$font_color = $b_hr->{'font'}->{'color'};
-		} elsif (exists $c_hr->{'font'}->{'color'}) {
-			$font_color = $c_hr->{'font'}->{'color'};
+		} elsif (exists $self->{'c'}->{'font'}->{'color'}) {
+			$font_color = $self->{'c'}->{'font'}->{'color'};
 		} else {
 			err "No font color for button '$button_nr'.";
 		}
 		my $font_size;
 		if (exists $b_hr->{'font'}->{'size'}) {
 			$font_size = $b_hr->{'font'}->{'size'};
-		} elsif (exists $c_hr->{'font'}->{'size'}) {
-			$font_size = $c_hr->{'font'}->{'size'};
+		} elsif (exists $self->{'c'}->{'font'}->{'size'}) {
+			$font_size = $self->{'c'}->{'font'}->{'size'};
 		} else {
 			err "No font size for button '$button_nr'.";
 		}
@@ -114,7 +127,7 @@ sub image {
 			err 'Cannot create font object.',
 				'Error', Imager->errstr;
 		}
-		my ($w, $h) = $self->_size($i);
+		($b_hr->{'w'}, $b_hr->{'h'}) = $self->_size($i);
 		my $color = Imager::Color->new($font_color);
 		my ($neg_width, $global_descent, $pos_width, $global_ascent,
 			$descent, $ascent, $advance_width, $right_bearing)
@@ -124,13 +137,13 @@ sub image {
 		if (exists $b_hr->{'text'}->{'pos'}->{'x'}) {
 			$x = $b_hr->{'text'}->{'pos'}->{'x'};
 		} else {
-			$x = $w / 2 - $advance_width / 2;
+			$x = $b_hr->{'w'} / 2 - $advance_width / 2;
 		}
 		my $y;
 		if (exists $b_hr->{'text'}->{'pos'}->{'y'}) {
 			$y = $b_hr->{'text'}->{'pos'}->{'y'};
 		} else {
-			$y = $h / 2 + $ascent / 2;
+			$y = $b_hr->{'h'} / 2 + $ascent / 2;
 		}
 		$i->string(
 			'aa' => 1,
@@ -150,7 +163,16 @@ sub image {
 	}
 
 	# Save.
-	$self->{'i'}->write('file' => $image);
+	my $ret = $self->{'i'}->write(
+		'file' => $image, 
+		defined $type ? (
+			'type' => $type,
+		) : (),
+	);
+	if (! defined $ret) {
+		err "Cannot write file to '$image'.",
+			'Error', Imager->errstr;
+	}
 
 	return;
 }
@@ -158,9 +180,52 @@ sub image {
 # Get image map.
 sub imagemap {
 	my $self = shift;
-	my $image_map = '';
-	# TODO
-	return $image_map;
+	my @image_map = (
+		['b', 'map'],
+		['a', 'name', 'keyboard'],
+	);
+	foreach my $button_nr (sort keys %{$self->{'c'}->{'button'}}) {
+		my $b_hr = $self->{'c'}->{'button'}->{$button_nr};
+		my $left = $b_hr->{'pos'}->{'left'};
+		my $top = $b_hr->{'pos'}->{'top'};
+		my $coords = join ',', ($left, $top, $left + $b_hr->{'w'},
+			$top + $b_hr->{'h'});
+		my $string = $b_hr->{'text'}->{'string'};
+		my $enc_string = $self->_encode_js($string);
+		push @image_map, (
+			['b', 'area'],
+			['a', 'id', 'button_'.$button_nr],
+			['a', 'shape', 'rect'],
+			['a', 'coords', $coords],
+			['a', 'onClick', "keyboard_click('$enc_string');"],
+			['e', 'area'],
+		);
+	}
+	push @image_map, (
+		['e', 'map'],
+	);
+	return @image_map;
+}
+
+# Encode values for js.
+sub _encode_js {
+	my ($self, $value) = @_;
+	if ($value eq '<--') {
+		$value = 'Backspace';
+	} elsif ($value eq decode_utf8('°')) {
+		$value = 'Ring';
+	} elsif ($value eq decode_utf8('ˇ')) {
+		$value = 'Caron';
+	} elsif ($value eq decode_utf8('¨')) {
+		$value = 'Diaeresis';
+	} elsif ($value eq decode_utf8('\'')) {
+		$value = 'Apostrophe';
+	} elsif ($value eq decode_utf8('´')) {
+		$value = 'Acute_accent';
+	}
+	my $enc_value = encode_entities($value);
+	$value = $enc_value;
+	return $value;
 }
 
 # Get sizes.
