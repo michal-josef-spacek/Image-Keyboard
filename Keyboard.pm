a44b43becf691707d02a507be1131a4353fc29e9
Michal Špaček <tupinek@gmail.com>
2013-07-11 10:43:02 +0200

    Fix config() method and setting.

--- Keyboard.pm
+++ Keyboard.pm
@@ -61,7 +61,7 @@ sub buttons {
 # Get or set configuration.
 sub config {
 	my ($self, $config_hr) = @_;
-	if ($config_hr) {
+	if (defined $config_hr) {
 		$self->{'config'} = $config_hr;
 		$self->_config_init;
 	}
