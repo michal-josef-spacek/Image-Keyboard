f1f786b948769d9dab17469baff03d0c90b358a5
Michal Špaček <tupinek@gmail.com>
2013-08-27 14:04:12 +0200

    Prepare for 0.04 version.

--- Keyboard.pm
+++ Keyboard.pm
@@ -14,7 +14,7 @@ use Imager;
 use List::MoreUtils qw(none);
 
 # Version.
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Constructor.
 sub new {
@@ -500,6 +500,6 @@ BSD license.
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
--- t/Image-Keyboard/01-version.t
+++ t/Image-Keyboard/01-version.t
@@ -8,4 +8,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($Image::Keyboard::VERSION, 0.03, 'Version.');
+is($Image::Keyboard::VERSION, 0.04, 'Version.');
